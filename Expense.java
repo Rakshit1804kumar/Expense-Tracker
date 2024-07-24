f5851cfaf3f26ca4e9ee7cf0bad1c3f9352b10b2
Rakshit1804kumar <147365205+Rakshit1804kumar@users.noreply.github.com>
2024-07-24 20:17:32 +0530

    Expense.java

---
+++ Expense.java
@@ -0,0 +1,25 @@
+import java.util.Date;
+
+public class Expense {
+    private Date date;
+    private String category;
+    private double amount;
+
+    public Expense(Date date, String category, double amount) {
+        this.date = date;
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
